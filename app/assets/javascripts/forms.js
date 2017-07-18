cc0cf166636240a51587ce6269d44a99fbb9686e
Xavi Blanch <xavi.blanch@season.es>
2017-07-18 19:27:19 +0200

    add basic clone behavior to the salad

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     e.preventDefault();
       $('#new-user-modal').modal('show',{backdrop:'static'});
   });
+
+  $('[data-control="key"]').wrap('<div class="clone-container"></div>');
+
+  $('[data-control="key"]').keypress(function(e){
+    if(e.charCode == 13){ // enter key
+      e.preventDefault();
+      if($(this).val()){ // don't wanna clone empty elements
+        $elem = $(this).clone(true,true);
+        $elem.val('');
+        $elem.appendTo($(this).parent());
+      }
+    } 
+  });
 });
