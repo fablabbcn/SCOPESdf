1f396bcc7891e6f82d7120d95acd5bcf4e24c3c5
Xavi Blanch <xavi.blanch@season.es>
2017-07-24 17:13:54 +0200

    on turbolinks ready instead of document ready

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -47,9 +47,7 @@ $(document).on('turbolinks:load', function() {
             }
         }
     });
-});
 
-$(document).ready(function(){
   $('#other-user-emails').click(function(e){
     e.preventDefault();
       $('#new-user-modal').modal('show',{backdrop:'static'});
