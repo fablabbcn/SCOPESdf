b968d965113d26132fbb2501d938bfafeabb7fed
Lucas Lorenzo Pena <Lucaslpena@gmail.com>
2017-07-25 17:04:39 +0200

    endpoint for file uploading

--- app/services/jq_uploader_service.rb
+++ app/services/jq_uploader_service.rb
@@ -6,7 +6,7 @@ class JqUploaderService
   def self.convert_to_jq_upload(carrier_item, lesson_id, symb)
     name = carrier_item.path.split("/").last
     thumbnail = '' unless carrier_item.respond_to?(:thumb)
-      {
+    json_obj = {
         "name" => name,
         "size" => carrier_item.size,
         "url" => carrier_item.url,
@@ -14,6 +14,7 @@ class JqUploaderService
         "delete_url" => "/lessons/#{lesson_id}/file/?name=#{name}&attr=#{symb}",
         "delete_type" => "DELETE"
     }
+    json_obj
   end
 
 
