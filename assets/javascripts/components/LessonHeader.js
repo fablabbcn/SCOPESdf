c67d4804198800095a83b69a573b56a9ee0a9c1c
Daniel Howells <daniel.howells@gmail.com>
2017-10-18 16:53:07 +0100

    Check for exsiatnce of slideshow before inits

--- assets/javascripts/components/LessonHeader.js
+++ assets/javascripts/components/LessonHeader.js
@@ -9,35 +9,40 @@ function LessonHeader() {
 	console.log("-- LessonHeader initialized")
 
 	let photosSelector = '.LessonHeader__photos'
-	let photoSelector = '.LessonHeader__photo'
+	let $photos = $(photosSelector)
 
-	let $status = $('.LessonHeader__photos__status')
-	let $current = $status.find('.current')
-	let $total = $status.find('.total')
+	if ($photos.length > 0) {
 
-  // Init flickity for all carousels
-  let flkty = new Flickity(photosSelector, {
-    cellAlign: 'center',
-    cellSelector: photoSelector,
-    contain: true,
-    pageDots: false,
-    prevNextButtons: false,
-    wrapAround: true,
-		imagesLoaded: true,
-    percentPosition: true,
-  })
+		let photoSelector = '.LessonHeader__photo'
 
-  document.addEventListener("turbolinks:request-start", function() {
-    flkty.destroy()
-  })
+		let $status = $('.LessonHeader__photos__status')
+		let $current = $status.find('.current')
+		let $total = $status.find('.total')
 
-	$total.html($(photoSelector).length)
+	  // Init flickity for all carousels
+	  let flkty = new Flickity(photosSelector, {
+	    cellAlign: 'center',
+	    cellSelector: photoSelector,
+	    contain: true,
+	    pageDots: false,
+	    prevNextButtons: false,
+	    wrapAround: true,
+			imagesLoaded: true,
+	    percentPosition: true,
+	  })
 
-	flkty.on( 'select', function() {
-		$current.html(flkty.selectedIndex + 1)
-	})
+	  document.addEventListener("turbolinks:request-start", function() {
+	    flkty.destroy()
+	  })
 
-  return flkty
+		$total.html($(photoSelector).length)
 
+		flkty.on( 'select', function() {
+			$current.html(flkty.selectedIndex + 1)
+		})
+
+	  return flkty
+
+	}
 
 }
