69ac295c946a25259aa34a5a68c4f385b1a93ffd
Roger Pales <roger.pales@vualto.com>
2017-12-04 23:58:29 +0100

    protect_from_forgery prepend: true, add passwords_coontroller (allow override devise default behaviour)

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,7 @@
+# Edit as needed
+#
+# source: devise-4.3.0/app/controllers/devise/passwords_controller.rb
+# default templates in app/views/devise/passwords/
+
+class PasswordsController < Devise::PasswordsController
+end
