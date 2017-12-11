0efed28fd7c4433bf087cdb5a1daf989816b4623
Daniel Howells <daniel.howells@gmail.com>
2017-12-11 16:02:04 +0000

    Few changes to how standards are handled

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
 class StandardsController < ApplicationController
 
   before_action :authenticate_user!
   # TODO - PUT PUNDIT
 
   before_action :set_lesson, only: [:index, :new, :show, :edit, :update, :destroy]
-  before_action :set_standards, only: [:new]
-  before_action :set_standard, only: [:show, :edit, :update, :destroy]
+  before_action :set_standards, only: [:new, :edit]
+  before_action :set_lessons_standard, only: [:show, :edit, :update, :destroy]
 
   def index
 
@@ -21,19 +22,37 @@ class StandardsController < ApplicationController
 
   end
 
+  def edit
+
+    # Assign the @form_step var, casting as integer instead of a string
+    @form_step = params[:form_step].present? ? params[:form_step].to_i : 1
+
+  end
+
   def create
     @lesson = Lesson.find(params["lesson_id"])
-    s = Standard.where(name:standard_params[:name]).first
+    s = Standard.where(name: standard_params[:name]).first
     l = LessonsStandard.new(description: standard_params[:description], index: standard_params[:index])
     l.lesson_id = @lesson.id
     l.standard_id = s.id
     l.save!
-    redirect_to edit_lesson_path(id: @lesson.id, form_step:3)
+    redirect_to edit_lesson_path(id: @lesson.id, form_step: 3)
   end
 
-  def delete
-    @lesson = Lesson.find(params["lesson_id"])
-    render :json => {deleted: @lesson.lessons_standards.where(index: standard_params[:index]).first.destroy!}
+  def destroy
+    
+    # If the standard isn't found, return to index
+    unless @lessons_standard_obj.present?
+      redirect_to lesson_standards_path(lesson_id: @lesson_obj.id)
+      return
+    end
+
+    # Destroy the step
+    @lessons_standard_obj.destroy
+
+    # Redirect to the lessons first step
+    redirect_to new_lesson_standard_path(lesson_id: @lesson_obj.id), notice: 'Your standard was deleted.'
+
   end
 
   private
@@ -46,12 +65,13 @@ class StandardsController < ApplicationController
       params.require(:name)
     end
 
-    def set_standard
-      @standard_obj = Standard.find_by_id(params[:id])
+    def set_lessons_standard
+      @lessons_standard_obj = LessonsStandard.includes(:standard).find_by(id: params[:id])
     end
 
-  def set_standards
-    @standards = Standard.all
-    #@standards = Standard.name_array 
-  end
+    def set_standards
+      @standards = Standard.all
+      #@standards = Standard.name_array
+    end
+
 end
